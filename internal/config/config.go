e6c9b36f69e5d58cd25a8350466747dc74dfc56c
Manuel Werder <manuelwerder@hotmail.com>
2026-05-04 00:58:36 +0200

    - Moved main.go file to cmd/agent/. - Adding Config Loading of yaml file or overrieds with env vars.

---
+++ internal/config/config.go
@@ -0,0 +1,17 @@
+// Package config
+package config
+
+type Config struct {
+	IntervalSeconds int            `yaml:"interval"`
+	QueueSize       int            `yaml:"queue_size"`
+	Exporter        ExporterConfig `yaml:"exporter"`
+}
+
+type ExporterConfig struct {
+	Type string     `yaml:"type"`
+	HTTP HTTPConfig `yaml:"http"`
+}
+
+type HTTPConfig struct {
+	Endpoint string `yaml:"endpoint"`
+}
---
+++ internal/config/loader.go
@@ -0,0 +1,76 @@
+// Package config
+package config
+
+import (
+	"errors"
+	"os"
+	"strconv"
+
+	"go.yaml.in/yaml/v4"
+)
+
+const defaultConfigPath = "./.config/msma/config.yaml"
+
+func Load() (*Config, error) {
+	cfg := defaultConfig()
+
+	if err := loadFromFile(cfg); err != nil {
+		return nil, err
+	}
+
+	if err := applyEnvOverrides(cfg); err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
+
+func defaultConfig() *Config {
+	return &Config{
+		IntervalSeconds: 5,
+		QueueSize:       10,
+		Exporter: ExporterConfig{
+			Type: "stdout",
+		},
+	}
+}
+
+func loadFromFile(cfg *Config) error {
+	data, err := os.ReadFile(defaultConfigPath)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+		return err
+	}
+
+	return yaml.Unmarshal(data, cfg)
+}
+
+func applyEnvOverrides(cfg *Config) error {
+	if interval := os.Getenv("AGENT_INTERVAL"); interval != "" {
+		integer, err := strconv.Atoi(interval)
+		if err != nil {
+			return err
+		}
+		cfg.IntervalSeconds = integer
+	}
+
+	if size := os.Getenv("AGENT_QUEUE_SIZE"); size != "" {
+		integer, err := strconv.Atoi(size)
+		if err != nil {
+			return err
+		}
+		cfg.QueueSize = integer
+	}
+
+	if eType := os.Getenv("AGENT_EXPORTER_TYPE"); eType != "" {
+		cfg.Exporter.Type = eType
+	}
+
+	if endpoint := os.Getenv("AGENT_HTTP_ENDPOINT"); endpoint != "" {
+		cfg.Exporter.HTTP.Endpoint = endpoint
+	}
+
+	return nil
+}
